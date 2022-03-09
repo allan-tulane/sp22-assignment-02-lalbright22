21a1c43a13dbf183910c9e612a57888b9e3dc68e
lalbright22 <98488193+lalbright22@users.noreply.github.com>
2022-03-09 18:03:45 +0000

    Trials

--- main.py
+++ main.py
@@ -15,35 +15,6 @@ class BinaryNumber:
     
 
 ## Implement multiplication functions here. Note that you will have to
-def _quadratic_multiply(x, y):
-  xvec, yvec = pad(x.binary_vec, y.binary_vec)
-  #if(len(xvec) <= 1 and len(yvec) <= 1):
-  if(x.decimal_val <= 1 and y.decimal_val <= 1):
-    return BinaryNumber(x.decimal_val*y.decimal_val)
-  else:
-    x_left, x_right = split_number(xvec)
-    y_left, y_right = split_number(yvec)
-
-    n = len(xvec)
-    #left part
-    LP = _quadratic_multiply(x_left, y_left)
-    sumLeft = bit_shift(LP, n)
-
-    #middle sum
-    MsumL = _quadratic_multiply(x_left, y_right)
-    MsumR = _quadratic_multiply(x_right, y_left)
-    inside = MsumL.decimal_val + MsumR.decimal_val
-    sumMid = bit_shift(BinaryNumber(inside), n//2)
-
-    #right sum
-    sumRight = _quadratic_multiply(x_right, y_right)
-
-    total = sumLeft.decimal_val + sumMid.decimal_val + sumRight.decimal_val
-    return BinaryNumber(total)
-
-def quadratic_multiply(x, y):
-  #converts the result from a binary number to a regular int
-  return _quadratic_multiply(x, y).decimal_val
     
 ## ensure that x, y are appropriately sized binary vectors for a
 ## divide and conquer approach.
@@ -77,13 +48,30 @@ def pad(x,y):
 
 def subquadratic_multiply(x, y):
     ### TODO
-  left_side = subquadratic_multiply(x_left, y_left)
-  right_side = subquadratic_multiply(x_right, y_right)
+  xvec, yvec = pad(x.binary_vec, y.binary_vec)
+  if(x.decimal_val <= 1 and y.decimal_val <= 1):
+    return BinaryNumber(x.decimal_val*y.decimal_val)
+  else:
+    x_left, x_right = split_number(xvec)
+    y_left, y_right = split_number(yvec)
 
-  xMid = BinaryNumber(x_left.decimal_val + y_left.decimal_val)
-  yMid = BinaryNumber(x_right.decimal_val + y_right.decimal_val)
+    n = len(xvec)
+    
+    lefts = subquadratic_multiply(x_left, y_left)
+    leftFin = bit_shift(lefts, n//2)
+    
+    rightFin = subquadratic_multiply(x_right, y_right)
+    #rightFin = bit_shift(rights, n)
 
-  middle = BinaryNumber(subquadratic_multiply(xMid, yMid).decimal_val-left_side.decimal_val-right_side.decimal_val)
+    xSums = BinaryNumber(x_left.decimal_val + x_right.decimal_val)
+    ySums = BinaryNumber(y_left.decimal_val + y_right.decimal_val)
+    xySums = xSums.decimal_val + ySums.decimal_val
+    xyFin = bit_shift(BinaryNumber(xySums), n//2)
+
+    final = xyFin.decimal_val - leftFin.decimal_val - rightFin.decimal_val
+    #final = BinaryNumber(subquadratic_multiply(xSums, ySums).decimal_val-lefts.decimal_val-rights.decimal_val)
+
+  return BinaryNumber(final)
     ###
 
 ## Feel free to add your own tests here.
